3ae6e4eaafed45eab03fb59357b55ef8c11b3646
MagdalenaAntkowiak <130896720+MagdalenaAntkowiak@users.noreply.github.com>
2024-05-18 21:29:28 +0200

    Update task-1.js

--- JS/task-1.js
+++ JS/task-1.js
@@ -7,3 +7,6 @@ console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+
+D:\Homework\goit-js-hw-03\js\task-1.js
